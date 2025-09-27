c5c42a24af4db5db7e9a6d3f50a2c10ed28e3b0a
GJ <joshi700@gmail.com>
2025-09-27 13:38:01 +0530

    Update HomePage.js

--- payment-app/src/HomePage.js
+++ payment-app/src/HomePage.js
@@ -192,7 +192,7 @@ function HomePage() {
         <div className="payment-card">
           <div className="payment-header">
             <div className="price-section">
-              <div className="price">Demo Payment of Hosted Checkout Product - $99.99</div>
+              <div className="price">Demo Payment - Hosted Checkout - Payment Page Option - $99.99</div>
             </div>
           </div>
 
